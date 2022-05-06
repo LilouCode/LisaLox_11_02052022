c263af0c9b7f78c33f6fa813514d6489a28db7c6
LilouCode <lisa.music@outlook.com>
2022-05-06 11:54:49 +0200

    Grid: add grid section to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import Banner from "../components/Banner"
+import Grid from "../components/Grid"
+
 
 function Home() {
     return (
       <div>
         <Banner/>
+        <Grid/>
       </div>
     )
   }
