c27eeb578515647ae68464856012d493f2340621
LilouCode <lisa.music@outlook.com>
2022-05-11 11:50:58 +0200

    feat (rental): add page details Add componments, title, description and user infos

--- src/pages/Rental.jsx
+++ src/pages/Rental.jsx
@@ -1,16 +1,49 @@
 import { useParams } from "react-router-dom";
 import {rentalsList} from "../datas/rentalsList"
+import Carrousel from "../components/Carrousel";
+import Dropdown from '../components/Dropdown';
+import Rate from "../components/Rate";
+import Tag from "../components/Tag";
 
 function Rental (){
     let {id} = useParams()
     let rental;
-    rental = rentalsList.filter(
+    const rentalId = rentalsList.filter(
         (rentals) => rentals.id.includes(id)
     );
+    rental = rentalId[0]
     console.log(rental)
     return(
         <div>
-            <h1>{rental[0].title}</h1>
+            <Carrousel/>
+            <div>
+                <div>
+                    <h1>{rental.title}</h1>
+                    <h2>{rental.location}</h2>
+                </div>
+                <div>
+                    <h3>{rental.host.name}</h3>
+                    <img src={rental.host.picture} alt={rental.host.name} />
+                </div>
+            </div>
+            <div>
+                <div>
+                    {rental.tags?.map((tag) => (
+                        <Tag key={tag + id} text= {tag}/>
+                    ))}
+                </div>
+                <Rate className = {"stars--" + rental.rating}/>
+            </div>
+
+            <div>
+                <Dropdown title ="Description" description={rental.description}/>
+                <Dropdown title= "Équipements" description= {<ul>
+                    {rental.equipments?.map((equipment) =>(
+                    <li key={rental.id + equipment}>{equipment}</li>
+                ))}
+                </ul>}/>
+            </div>
+            
         </div>
     )
 }
