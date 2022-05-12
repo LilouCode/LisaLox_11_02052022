df884d6a21b89e5fd412d5c21d3502ad7244e82e
LilouCode <lisa.music@outlook.com>
2022-05-12 12:42:56 +0200

    style (rental): add carrousel component style Add the arrow svg files and create a carrousel scss file

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,10 +3,11 @@ function Carrousel (props){
     const [pictureShown, changePicture] = useState(0);
     
     return(
-        <div>
-            <button onClick={ () => changePicture(pictureShown===0? props.pics.length -1: pictureShown -1)}></button>
+        <div className="carrousel">
+            <button className="carrousel__btn carrousel__btn__prev" onK onClick={ () => changePicture(pictureShown===0? props.pics.length -1: pictureShown -1)}></button>
             <img src={props.pics[pictureShown]} alt={props.alt} />
-            <button onClick={ () => changePicture(pictureShown===props.pics.length - 1? 0: pictureShown +1)}></button>
+            <h6>{(pictureShown +1) +"/"+(props.pics.length)}</h6>
+            <button className="carrousel__btn carrousel__btn__next" onClick={ () => changePicture(pictureShown===props.pics.length - 1? 0: pictureShown +1)}></button>
         </div>
     )
 }
