7f48af860be4806d42ec0f6ce0de0d0ec1ee5a1f
LilouCode <lisa.music@outlook.com>
2022-05-12 12:47:48 +0200

    fix (carrousel): delete type error

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -4,7 +4,7 @@ function Carrousel (props){
     
     return(
         <div className="carrousel">
-            <button className="carrousel__btn carrousel__btn__prev" onK onClick={ () => changePicture(pictureShown===0? props.pics.length -1: pictureShown -1)}></button>
+            <button className="carrousel__btn carrousel__btn__prev" onClick={ () => changePicture(pictureShown===0? props.pics.length -1: pictureShown -1)}></button>
             <img src={props.pics[pictureShown]} alt={props.alt} />
             <h6>{(pictureShown +1) +"/"+(props.pics.length)}</h6>
             <button className="carrousel__btn carrousel__btn__next" onClick={ () => changePicture(pictureShown===props.pics.length - 1? 0: pictureShown +1)}></button>
