4bfd8b63ac34312ad1d9caa3725f17fc9c99d5dd
LilouCode <lisa.music@outlook.com>
2022-05-12 13:12:43 +0200

    build: upgrade to React 18

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
-import { render } from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './pages/App';
+import App from "./pages/App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Rental from "./pages/Rental";
-// import { rentalsList } from "./datas/rentalsList";
-// import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById("root");
-render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="rental" element={<Rental/>} />
-        <Route path="rental/:id" element={<Rental/>} />
-        
-        <Route 
-      path="*"
-      element={<Error />}/>
+        <Route path="rental" element={<Rental />} />
+        <Route path="rental/:id" element={<Rental />} />
+        <Route path="*" element={<Error />} />
       </Route>
     </Routes>
-  </BrowserRouter>,
-  rootElement
+  </BrowserRouter>
 );
