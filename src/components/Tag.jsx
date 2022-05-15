34cc6e667bfaa3b4b98872804152b3509af6d312
LilouCode <lisa.music@outlook.com>
2022-05-15 11:18:19 +0200

    feat (tag): adapt tags with many characters

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,6 +1,7 @@
 function Tag(props) {
+    console.log(props.text.length)
     return(
-        <h5 className="tag">{props.text}</h5>
+        <h5 className={(props.text.length < 13? "tag": "tag tag--XL")}>{props.text}</h5>
     )
 } 
 export default Tag
